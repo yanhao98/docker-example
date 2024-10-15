b7df52a2b00ac57c034891180388817e5b56b856
严浩 <me@yanhao.ren>
2024-10-15 17:25:58 +0800

    duplicati demo

---
+++ duplicati/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# https://github.com/linuxserver/docker-duplicati
+# https://github.com/duplicati/duplicati/blob/master/ReleaseBuilder/Resources/Docker/README.md
+
+DOMAIN=❗️❗️❗️
+PASSWORD=❗️❗️❗️
+
+docker run -d -p 8500:8200 --name duplicati_canary \
+  -v duplicati-data:/data \
+  -v ~/_docker-stacks:/_docker-stacks \
+  -e "TZ=Asia/Shanghai" \
+  -e "DUPLICATI__WEBSERVICE_PASSWORD=$PASSWORD" \
+  -e "DUPLICATI__WEBSERVICE_ALLOWED_HOSTNAMES=$DOMAIN" \
+  duplicati/duplicati:canary
+docker logs -f duplicati_canary
